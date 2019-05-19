898e4dfd8f0da69f7c14617251223a7566277869
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-19 18:44:45 +0100

    Create Student Model and validations

---
+++ app/models/student.rb
@@ -0,0 +1,29 @@
+class Student < ApplicationRecord
+    # Includes
+
+    # Attributes
+    
+    # Associations
+
+    # Delegates
+
+    # Constants
+    VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+
+    # Validations
+    validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 },
+                                    format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: true }
+
+    # Scopes
+
+    # Callbacks
+    before_save { self.email = email.downcase }
+    
+    # Constants Methods
+
+    # Default
+
+    private
+end
