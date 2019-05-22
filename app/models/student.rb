408343769104d9192591d9587057c765e76f64d4
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-22 22:01:03 +0100

    Add secure password(password_digest)

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ApplicationRecord
+    has_secure_password
+    
     # Includes
 
     # Attributes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_19_173820) do
+ActiveRecord::Schema.define(version: 2019_05_22_205738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_05_19_173820) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
