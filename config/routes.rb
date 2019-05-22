2211a3d522b43c1aeff3ee139d26a1bd0e259c77
Hugo Morais <hugodanielmorais@gmail.com>
2019-05-22 23:12:12 +0100

    Login and Logout system

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'courses#index' 
   
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
+  
   resources :courses
   resources :students
   get 'about', to: 'pages#about'
